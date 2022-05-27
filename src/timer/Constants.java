04a4c361d950d3dd5fbc4e123c154948be0c6103
Ivan Zou <ivanzou2@gmail.com>
2022-05-27 10:13:37 -0400

    added to Constants

--- src/timer/Constants.java
+++ src/timer/Constants.java
@@ -6,6 +6,11 @@ public class Constants {
     public static final int WINDOW_WIDTH = 550;
     public static final int WINDOW_HEIGHT = 175;
 
+    public static final int POPUP_LOCATION_X = 475;
+    public static final int POPUP_LOCATION_Y = 235;
+    public static final int POPUP_WIDTH = 400;
+    public static final int POPUP_HEIGHT = 105;
+
     public static final int REFRESH_RATE = 120;
 
     public static final int TIME_FONT_SIZE = 75;
@@ -15,4 +20,9 @@ public class Constants {
     public static final int SECONDS_PER_MINUTE = 60;
     public static final int SECONDS_PER_HOUR = 3600;
     public static final int MINUTES_PER_HOUR = 60;
+
+    public static final String SHORT_BLANK_STRING = "           ";
+    public static final String LONG_BLANK_STRING = "                     ";
+
+    public static final String AUDIO_FILE = "";
 }
