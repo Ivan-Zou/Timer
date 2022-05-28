b29f8b1616b240c5b6f7e97820238f7f52414c5c
Ivan Zou <ivanzou2@gmail.com>
2022-05-28 13:05:48 -0400

    added the Sound class

---
+++ src/timer/classes/Sound.java
@@ -0,0 +1,28 @@
+package timer.classes;
+
+import timer.misc.Constants;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.SwingUtilities;
+import javax.swing.JOptionPane;
+import java.io.File;
+
+public class Sound {
+    public static void play() {
+        try {
+            Clip clip = AudioSystem.getClip();
+            File audioFile = new File(Constants.AUDIO_FILE);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile);
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            SwingUtilities.invokeLater(() -> {
+                JOptionPane.showMessageDialog(null, "STOP");
+                System.exit(0);
+            });
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
